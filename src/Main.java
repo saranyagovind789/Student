88680dcddec2afb203107b8e690fc7f02925c70d
Saranya Govind <saranya789@gmail.com>
2023-01-30 22:34:35 +0000

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static  void main (String[] args){
+        Student student = new Student("Saranya", 'C', 3);
+
+        System.out.println("Name :"+student.getName());
+        System.out.println("Grade: "+student.getGrade());
+    }
+}
---
+++ src/Student.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class Student {
+    private String name;
+    private Character grade;
+    private Integer group;
+    private final String secretNickName = "MySecretNickName";
+
+    private final List<Character> grades = Arrays.asList('A', 'B', 'C', 'D', 'E', 'F');
+    private final List<Integer> groups = Arrays.asList(1, 2, 3, 4, 5);
+
+    public Student(String name, char grade, int group) {
+        this.name = name;
+        this.grade = grade;
+        this.group = group;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public char getGrade(){
+        return grade;
+    }
+
+    public void downgrade(){
+        grade = grades.get(grades.indexOf(grade) +1);
+    }
+
+    public void upgrade(){
+        grade = grades.get(grades.indexOf(grade) -1);
+    }
+}
